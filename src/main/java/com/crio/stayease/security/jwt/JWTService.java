a61a3b65cad1d93811cbcd4a631fca2fad6605cf
Chaitya W <chaityaw@Chaityas-MacBook-Air.local>
2024-07-04 14:03:02 +0530

    Made Changes to JWTService

--- src/main/java/com/crio/stayease/security/jwt/JWTService.java
+++ src/main/java/com/crio/stayease/security/jwt/JWTService.java
@@ -9,8 +9,7 @@ import java.util.Date;
 
 @Service
 public class JWTService {
-    Dotenv dotenv = Dotenv.configure().load();
-    String alogSecret = dotenv.get("1b31239gebd982123???{}(@^$#)");
+    String alogSecret = "1b31239gebd982123???{}(@^$#)";
 
     private final Algorithm algorithm = Algorithm.HMAC256(alogSecret);
 
