72c29e259dd46f4d0966af87c48bcd42c58478b1
Chaitya W <chaityaw@Chaityas-MacBook-Air.local>
2024-07-04 13:16:51 +0530

    Added new DB

--- src/main/java/com/crio/stayease/models/BaseModel.java
+++ src/main/java/com/crio/stayease/models/BaseModel.java
@@ -3,14 +3,16 @@ package com.crio.stayease.models;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseModel {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
 
     @CreatedDate
